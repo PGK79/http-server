8e05a255d0c713b523117eda882de97f260cc9e9
PGK79 <pgk_nev@mail.ru>
2022-12-16 13:39:14 +0500

    Добавлена код вызова обработчиков из задачи

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -3,5 +3,19 @@ package ru.netology;
 public class Main {
     public static void main(String[] args) {
         final var server = Server.getInstance();
+
+        // добавление handler'ов (обработчиков)
+        server.addHandler("GET", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
+                // TODO: handlers code
+            }
+        });
+        server.addHandler("POST", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
+                // TODO: handlers code
+            }
+        });
+
+        server.listen(9999);
     }
 }
