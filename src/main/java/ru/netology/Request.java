3feb9f9d0db7db9bfa6dac2b3e85a1b186121bd8
PGK79 <pgk_nev@mail.ru>
2022-12-16 22:18:54 +0500

    Добавлены поля и конструкторы Request

--- src/main/java/ru/netology/Request.java
+++ src/main/java/ru/netology/Request.java
@@ -1,4 +1,19 @@
 package ru.netology;
 
+import java.util.List;
+
 public class Request {
+    private String verb;
+    private List<String> headers;
+    private String body;
+
+    public Request(String verb, List<String> headers) {
+        this.verb = verb;
+        this.headers = headers;
+    }
+    public Request(String verb, List<String> headers, String body) {
+        this.verb = verb;
+        this.headers = headers;
+        this.body = body;
+    }
 }
