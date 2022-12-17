02190d5a1d43927ba2416afe284490ac8ea9884f
PGK79 <pgk_nev@mail.ru>
2022-12-17 13:03:11 +0500

    Добавлен сетер Body

--- src/main/java/ru/netology/Request.java
+++ src/main/java/ru/netology/Request.java
@@ -4,15 +4,18 @@ import java.util.List;
 
 public class Request {
     private String verb;
+    private String path;
     private List<String> headers;
     private String body;
 
-    public Request(String verb, List<String> headers) {
+    public Request(String verb, String path, List<String> headers) {
         this.verb = verb;
+        this.path = path;
         this.headers = headers;
     }
-    public Request(String verb, List<String> headers, String body) {
+    public Request(String verb, String path, List<String> headers, String body) {
         this.verb = verb;
+        this.path = path;
         this.headers = headers;
         this.body = body;
     }
@@ -21,8 +24,20 @@ public class Request {
         return verb;
     }
 
+    public String getBody() {
+        return body;
+    }
+    public String getPath() {
+        return path;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
     @Override
     public String toString() {
-        return "Метод: " + verb + "\n" + "Заголовки:\n " + headers + "\nТело:\n " + body;
+        return  "Метод: " + verb + "\n"+ "Путь: " + path + "\n" + "Заголовки:\n " + headers
+                + "\nТело:\n " + body;
     }
 }
