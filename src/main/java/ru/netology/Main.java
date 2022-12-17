a31de7ec1969978bf1a9d4f189a2eb69919ceff5
PGK79 <pgk_nev@mail.ru>
2022-12-17 13:06:06 +0500

    Handler for Get message

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -2,6 +2,8 @@ package ru.netology;
 
 import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.concurrent.ExecutionException;
 
 public class Main {
@@ -9,14 +11,28 @@ public class Main {
         final var server = Server.getInstance();
 
         server.addHandler("GET", "/messages", new Handler() {
-            public void handle(Request request, BufferedOutputStream responseStream) {
-                // TODO: handlers code
+            public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
+                // У GET запроса тела нет, а условия задачи не ограничивают что отправить в теле ответа
+                request.setBody("Very important information");
+                String content = request.getBody();
 
+                final var mimeType = "text/plain";
+                final var length = content.length();
+                responseStream.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                responseStream.write(content.getBytes());
+                responseStream.flush();
             }
         });
         server.addHandler("POST", "/messages", new Handler() {
             public void handle(Request request, BufferedOutputStream responseStream) {
                 // TODO: handlers code
+                System.out.println("POST");
             }
         });
 
